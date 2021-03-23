200273c849fe55720674cfb01da861631faf6c7d
AbdouBouallaga <abdelkader.bouallaga@um5s.net.ma>
2021-03-23 12:34:52 +0100

    stereocopic effect fixed

--- src/raytracing/stereoscopy.c
+++ src/raytracing/stereoscopy.c
@@ -6,11 +6,19 @@
 /*   By: babdelka <babdelka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 10:34:17 by yait-el-          #+#    #+#             */
-/*   Updated: 2021/03/22 18:15:28 by babdelka         ###   ########.fr       */
+/*   Updated: 2021/03/23 11:59:45 by babdelka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
+
+int					isblack(t_vector color)
+{
+	if (color.x == 0 && color.x == 0 && color.x == 0 )
+		return(1);
+	else
+		return(0);
+}
 
 void				stereoscopy(t_rtv *rtv, double x, double y, t_ray ray2)
 {
@@ -25,13 +33,21 @@ void				stereoscopy(t_rtv *rtv, double x, double y, t_ray ray2)
 	test = (t_vector){0, 0, 0};
 	i = 0;
 	ray2.direction = nrm(camera(rtv->camera, x + 15, y, test));
-	color = add(color, get_pxl(rtv, ray2));
-	color.x = 0;
-	color.y += 50;
+	color = get_pxl(rtv, ray2);
+	if (!isblack(color))
+	{
+		color.y = 0;
+		color.x+= 60;
+	}
 	ray2.direction = nrm(camera(rtv->camera, x - 15, y, test));
-	color2 = add(color, get_pxl(rtv, ray2));
-	color2.x += 50;
-	color2.y = 0;
-	color = multi(add(color, color2), 0.5);
+	color2 = get_pxl(rtv, ray2);
+	if (!isblack(color2))
+	{
+		color2.x = 0;
+		color2.y += 60;
+	}
+	color.x = (color.x + color2.x) / 2;
+	color.y = (color.y + color2.y) / 2;
+	color.z = (color.z + color2.z) / 2;
 	rtv->mlx.img[(WIN_H - 1 - (int)y) * WIN_W + (int)x] = rgb_to_int(color);
 }
